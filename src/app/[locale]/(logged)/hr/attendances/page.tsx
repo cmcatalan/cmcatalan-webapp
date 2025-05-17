f97acf5c938c7f3916a2323fd522166d3dd37d9d
Miguel Catalan <17494657+cmcatalan@users.noreply.github.com>
2025-05-18 01:34:04 +0200

    fix: error time

--- src/app/[locale]/(logged)/hr/attendances/page.tsx
+++ src/app/[locale]/(logged)/hr/attendances/page.tsx
@@ -10,6 +10,7 @@ import {addDays, differenceInMinutes, format} from "date-fns";
 import {orderBy} from "lodash";
 import Link from "next/link";
 import {Button} from "@heroui/react";
+import { TZDate } from "@date-fns/tz";
 
 interface AttendancesPageProps {
     searchParams: Promise<{ date?: string }>
@@ -33,7 +34,7 @@ export default async function AttendancesPage({searchParams}: AttendancesPagePro
         const month = parseInt(splits[1]!) - 1;
         const day = parseInt(splits[2]!);
 
-        parsedDate = new Date(year, month, day, 0, 0, 0, 0);
+        parsedDate = new TZDate(year, month, day, defaultTimeZone);
     }
 
     const parsedStart = toZonedTime(parsedDate, defaultTimeZone);
