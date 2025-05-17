bbe958fcc48031c7f88c775276ac4e4b5fd55c31
Miguel Catalan <17494657+cmcatalan@users.noreply.github.com>
2025-05-17 12:22:32 +0200

    feature: manager screens

---
+++ src/app/[locale]/(logged)/manager/layout.tsx
@@ -0,0 +1,11 @@
+import {auth} from "@/auth";
+import {redirect} from "next/navigation";
+import {ReactNode} from "react";
+
+export default async function EmployeeLayout({children}: { children: ReactNode }) {
+    const session = await auth();
+    if (!session?.user || !session.user.roles.includes("Manager"))
+        redirect("/");
+
+    return <>{children}</>;
+}
